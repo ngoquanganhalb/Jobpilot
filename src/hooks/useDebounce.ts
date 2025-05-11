f360ea31806f4f57e989eec354f1fa1dbf0b40ca
ngoquanganhalb <ngoquanganhalb@gmail.com>
2025-05-11 19:22:57 +0700

    8

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useDebounce(value, delay = 500) {
+export default function useDebounce(value : any, delay = 500) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
